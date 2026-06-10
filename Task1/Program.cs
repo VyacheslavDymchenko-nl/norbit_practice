a24837056e219c642e07af103106e34bd1d2abbf
tomiokao3o <slava.dymchenko@mail.ru>
2026-06-10 19:31:39 +0400

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountFutureValues(initialDeposit: 1000, years: 3, interestRate: 10));
+        }
+
+        /// <summary>
+        /// Формирует и возвращает строку с расчетом сложных процентов по годам.
+        /// </summary>
+        /// <param name="initialDeposit">Начальный вклад.</param>
+        /// <param name="years">Количество лет.</param>
+        /// <param name="interestRate">Годовая процентная ставка.</param>
+        /// <returns>Строка, где для каждого года расчета указана сумма накоплений.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static string CountFutureValues(decimal initialDeposit, int years, decimal interestRate)
+        {
+            if (initialDeposit <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Начальный вклад должен быть положительным!");
+            }
+            if (years <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Количество лет должно быть положительным!");
+            }
+            if (interestRate <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Годовая процентная ставка должна быть положительной!");
+            }
+
+            var result = new StringBuilder();
+            var growthFactor = interestRate / 100m + 1m;
+
+            for (int i = 0; i < years; i++)
+            {
+                initialDeposit *= growthFactor;
+                result.Append($"Год {i + 1}: {initialDeposit:F2} руб.\n");
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDiamond(5);
+        }
+
+        /// <summary>
+        /// Выводит на экран ромб из символов X.
+        /// </summary>
+        /// <param name="diagonalLength">Длина диагонали ромба.</param>
+        public static void PrintDiamond(int diagonalLength)
+        {
+            if (diagonalLength <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Длина диагонали должна быть положительной!");
+            }
+            if (diagonalLength %2 == 0)
+            {
+                throw new ArgumentOutOfRangeException("Длина диагонали должна быть нечетной!");
+            }
+
+            var result = new StringBuilder();
+
+            for (int i = 0; i < diagonalLength; i++)
+            {
+                result.Append(' ', diagonalLength);
+                result.Append("\r\n");
+
+                int rowCentre = i * (diagonalLength + 2) + diagonalLength / 2;
+                int offset = diagonalLength / 2 - Math.Abs(i - diagonalLength / 2);
+
+                result[rowCentre + offset] = 'X';
+                result[rowCentre - offset] = 'X';
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
+
